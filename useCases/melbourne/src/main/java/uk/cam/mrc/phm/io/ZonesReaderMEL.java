6be7820af03d001048029eb300209dcf91813558
Carl Higgs <carl.higgs@rmit.edu.au>
2025-05-23 13:08:58 +1000

    addresses #22

--- useCases/melbourne/src/main/java/uk/cam/mrc/phm/io/ZonesReaderMEL.java
+++ useCases/melbourne/src/main/java/uk/cam/mrc/phm/io/ZonesReaderMEL.java
@@ -13,6 +13,8 @@ import org.geotools.api.feature.simple.SimpleFeature;
 import org.locationtech.jts.geom.Geometry;
 import org.matsim.core.utils.gis.ShapeFileReader;
 
+import java.util.Arrays;
+
 
 /**
  * @author Nico
@@ -22,6 +24,7 @@ public class ZonesReaderMEL extends AbstractCsvReader {
     private static final Logger logger = LogManager.getLogger(ZonesReaderMEL.class);
     private int idIndex;
     private int areaTypeIndex;
+    private static final String ZONE_ID = Resources.instance.getString(Properties.ZONE_SHAPEFILE_ID_FIELD);
 
     public ZonesReaderMEL(DataSet dataSet) {
         super(dataSet);
@@ -35,7 +38,7 @@ public class ZonesReaderMEL extends AbstractCsvReader {
 
     private static void mapFeaturesToZones(DataSet dataSet) {
         for (SimpleFeature feature: ShapeFileReader.getAllFeatures(Resources.instance.getZoneShapesInputFile().toString())) {
-            int zoneId = Integer.parseInt(feature.getAttribute(Resources.instance.getString(Properties.ZONE_SHAPEFILE_ID_FIELD)).toString());
+            int zoneId = Integer.parseInt(feature.getAttribute(ZONE_ID).toString());
             MitoZone zone = dataSet.getZones().get(zoneId);
             if (zone != null){
                 zone.setGeometry((Geometry) feature.getDefaultGeometry());
@@ -47,8 +50,9 @@ public class ZonesReaderMEL extends AbstractCsvReader {
 
     @Override
     protected void processHeader(String[] header) {
-        idIndex = MitoUtil.findPositionInArray("oaID", header);
-        areaTypeIndex = MitoUtil.findPositionInArray("urbanType", header);
+        String[] cleanHeader = Arrays.stream(header).map(h -> h.replace("\"", "").trim()).toArray(String[]::new);
+        idIndex = MitoUtil.findPositionInArray(ZONE_ID,cleanHeader);
+        areaTypeIndex = MitoUtil.findPositionInArray("urbanType", cleanHeader);
     }
 
     @Override
