0a576be1509361a453d9a446174186ab88139a14
Carl Higgs <carl.higgs@rmit.edu.au>
2025-06-02 16:21:01 +1000

    updated active mode coefficient parsing towards #1 and #32; female coefficient shouldn't apply for children

--- useCases/melbourne/src/main/java/uk/cam/mrc/phm/RunMatsimActiveMode.java
+++ useCases/melbourne/src/main/java/uk/cam/mrc/phm/RunMatsimActiveMode.java
@@ -253,7 +253,7 @@ public class RunMatsimActiveMode {
             speed += extractCoefficient(purpose, mode, "speed");
 
             // Interaction terms
-            if (gender.equals(MitoGender.FEMALE)) {
+            if (age >= 16 && gender.equals(MitoGender.FEMALE)) {
                 grad += extractCoefficient(purpose, mode, "grad_f");
                 stressLink += extractCoefficient(purpose, mode, "stressLink_f");
                 vgvi += extractCoefficient(purpose, mode, "vgvi_f");
