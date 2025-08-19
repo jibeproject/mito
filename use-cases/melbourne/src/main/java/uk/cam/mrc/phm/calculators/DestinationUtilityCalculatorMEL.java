eaf393ab13747f3def9d299d40a13bf9f83897b0
Carl Higgs <carlhiggs@gmail.com>
2025-08-19 01:31:17 +0000

    updated pre-calibration values, towards #76

--- use-cases/melbourne/src/main/java/uk/cam/mrc/phm/calculators/DestinationUtilityCalculatorMEL.java
+++ use-cases/melbourne/src/main/java/uk/cam/mrc/phm/calculators/DestinationUtilityCalculatorMEL.java
@@ -4,38 +4,44 @@ import de.tum.bgu.msm.data.*;
 import de.tum.bgu.msm.modules.tripDistribution.AbstractDestinationUtilityCalculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class DestinationUtilityCalculatorMEL extends AbstractDestinationUtilityCalculator {
 
-    private final static double[] TRAVEL_DISTANCE_PARAM_HBW = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // HBW mean: (-0.10565083158495737 + -0.04366834773482148 + -0.029824447609604468) / 3 = -0.05971454230979444
+    private final static double[] TRAVEL_DISTANCE_PARAM_HBW = fillArray(-0.05971454230979444, 10);
     private final static double IMPEDANCE_PARAM_HBW = 9;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_HBE = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // HBE mean: (-0.05660650656401385 + -0.04730781708082465 + -0.048208428932957896) / 3 = -0.05004058485955447
+    private final static double[] TRAVEL_DISTANCE_PARAM_HBE = fillArray(-0.05004058485955447, 10);
     private final static double IMPEDANCE_PARAM_HBE = 28.3;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_HBA = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // HBA mean: (-0.12740481694077424 + -0.03845635376344552 + -0.03688687152506809) / 3 = -0.06791668040942928
+    private final static double[] TRAVEL_DISTANCE_PARAM_HBA = fillArray(-0.06791668040942928, 10);
     private final static double IMPEDANCE_PARAM_HBA = 28.3;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_HBS = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // HBS mean: (-0.1302481725479954 + -0.0716719631663481 + -0.06272342097190081) / 3 = -0.08888185289574877
+    private final static double[] TRAVEL_DISTANCE_PARAM_HBS = fillArray(-0.08888185289574877, 10);
     private final static double IMPEDANCE_PARAM_HBS = 14.5;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_HBR = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // HBR mean: (-0.09341006840787956 + -0.046114237946687356 + -0.03142021825706931) / 3 = -0.05764850820354541
+    private final static double[] TRAVEL_DISTANCE_PARAM_HBR = fillArray(-0.05764850820354541, 10);
     private final static double IMPEDANCE_PARAM_HBR = 20;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_HBO = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // HBO mean: (-0.028762939944400743 + -0.013710304239040036 + -0.010043876974845265) / 3 = -0.017172707052428348
+    private final static double[] TRAVEL_DISTANCE_PARAM_HBO = fillArray(-0.017172707052428348, 10);
     private final static double IMPEDANCE_PARAM_HBO = 53;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_RRT = {-0.12877572260971806}; //redundant?
-    private final static double IMPEDANCE_PARAM_RRT = 20; //redundant?
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_NHBW = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    //    private final static double[] TRAVEL_DISTANCE_PARAM_RRT = {-0.12877572260971806}; //redundant?
+    //    private final static double IMPEDANCE_PARAM_RRT = 20; //redundant?
+    // NHBW mean: (-0.38036997226527924 + -0.05575584617535517 + -0.02997957948287176) / 3 = -0.15570113264116872
+    private final static double[] TRAVEL_DISTANCE_PARAM_NHBW = fillArray(-0.15570113264116872, 10);
     private final static double IMPEDANCE_PARAM_NHBW = 15.1;
-
-    private final static double[] TRAVEL_DISTANCE_PARAM_NHBO = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    // NHBO mean: (-0.26157597149372586 + -0.04848404658490696 + -0.042259625478948036) / 3 = -0.11743988151986029
+    private final static double[] TRAVEL_DISTANCE_PARAM_NHBO = fillArray(-0.11743988151986029, 10);
     private final static double IMPEDANCE_PARAM_NHBO = 20;
 
+    private static double[] fillArray(double value, int length) {
+        double[] arr = new double[length];
+        for (int i = 0; i < length; i++) arr[i] = value;
+        return arr;
+    }
 
     public DestinationUtilityCalculatorMEL(Purpose purpose) {
 
